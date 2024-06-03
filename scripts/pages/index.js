3801537c0fdb565c42fb0d5760a32f92d1e974cd
zafle <fleutotanne@free.fr>
2024-06-03 15:54:24 +0200

    add medias

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,5 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
             {
@@ -41,6 +41,5 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
-    
+
     init();
-    
