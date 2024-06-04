ff93910bb0efd51531c3cfb9c7af815e6f4a4e11
zafle <fleutotanne@free.fr>
2024-06-04 17:01:52 +0200

    homepage CSS/Sass - general Sass

--- scripts/templates/photographerCard.js
+++ scripts/templates/photographerCard.js
@@ -11,12 +11,12 @@ class PhotographerCard {
 
     createPhotographerCard() {
         const PhotographerCard = `
-            <a href="photographer.html?id=${this._photographer.id}" title="Voir la page de ${this._photographer.name}" target="_self">
+            <a class="photographer__link" href="photographer.html?id=${this._photographer.id}" title="Voir la page de ${this._photographer.name}" target="_self">
                 <div class="photographer__img-mask">
                     <img src="${this._photographer.portrait}" alt="${this._photographer.name}"/>
                 </div>
                 <h2 class="photographer__name">${this._photographer.name}</h2>
-            <a/>
+            </a>
             <p class="photographer__location">${this._photographer.city}, ${this._photographer.country}</p>
             <p class="photographer__tagline">${this._photographer.tagline}</p>
             <p class="photographer__price">${this._photographer.price}</p>
