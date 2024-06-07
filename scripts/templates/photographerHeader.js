1fc495165058fdb93ccd66b25979eb5c00778092
zafle <fleutotanne@free.fr>
2024-06-07 08:33:51 +0200

    CSS + HTML buttons and media filter

--- scripts/templates/photographerHeader.js
+++ scripts/templates/photographerHeader.js
@@ -6,34 +6,35 @@ class PhotographerHeader {
     constructor(photographer) {
         this._photographer = photographer
         this.$header = document.querySelector('.photograph-header')
-        this.$infos = document.createElement('div')
-        this.$portrait = document.createElement('div')
     }
 
     // create HTML content for photographer's header
     createHeader() {
-        this.$infos.innerHTML = `
-            <h1 class="photograph-header__title">
-                ${this._photographer.name}
-            </h1>
-            <p class="photograph-header__location">
-                ${this._photographer.city}, ${this._photographer.country}
-            </p>
-            <p class="photograph-header__tagline">
-                ${this._photographer.tagline}
-            </p>
-        `
-        this.$portrait.innerHTML = `
-            <div class="photographer__img-mask">
-                <img src="${this._photographer.portrait}"
-                alt="${this._photographer.name}"/>
+        this.$header.innerHTML = `
+            <div class="photograph-header__infos photograph-header__item">
+                <h1 class="photograph-header__title">
+                    ${this._photographer.name}
+                </h1>
+                <p class="photograph-header__location">
+                    ${this._photographer.city}, ${this._photographer.country}
+                </p>
+                <p class="photograph-header__tagline">
+                    ${this._photographer.tagline}
+                </p>
+            </div>
+            <button
+                class="btn-style btn-hover photograph-header__item"
+                aria-label="Contact me"
+                data-id="${this._photographer.id}"
+                onclick="displayModal()">
+                Contactez-moi
+            </button>
+            <div class="photograph-header__portrait photograph-header__item">
+                <div class="photographer__img-mask">
+                    <img src="${this._photographer.portrait}"
+                    alt="${this._photographer.name}"/>
+                </div>
             </div>
         `
-
-        this.$infos.classList.add('photograph-header__infos', 'photograph-header__item')
-        this.$portrait.classList.add('photograph-header__portrait', 'photograph-header__item')
-
-        this.$header.prepend(this.$infos)
-        this.$header.append(this.$portrait)
     }
 }
